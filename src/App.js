dcbad7a5b56c10fef96db039d9950d7e01211f44
saman-zdf <samanfyz68@gmail.com>
2022-02-22 16:56:10 +0800

    setup basic redux, like reducer and initialState

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
-import React from "react";
+import React from 'react';
 // components
-import Navbar from "./components/Navbar";
-import CartContainer from "./components/CartContainer";
+import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
 // items
-import cartItems from "./cart-items";
+import cartItems from './cart-items';
 // redux stuff
-
+import { createStore } from 'redux';
+// initial store
+const initialStore = {
+  count: 0,
+};
+// reducer
+function reducer(state, action) {
+  console.log({ state, action });
+  if (action.type === 'DECREASE') {
+    return {
+      count: state.count - 1,
+    };
+  }
+  if (action.type === 'INCREASE') {
+    return {
+      count: state.count + 1,
+    };
+  }
+  if (action.type === 'RESET') {
+    return {
+      count: 0,
+    };
+  }
+  return state;
+}
+const store = createStore(reducer, initialStore);
+store.dispatch({ type: 'DECREASE' });
+store.dispatch({ type: 'INCREASE' });
+store.dispatch({ type: 'INCREASE' });
+store.dispatch({ type: 'INCREASE' });
+store.dispatch({ type: 'RESET' });
+console.log(store.getState());
 function App() {
   // cart setup
 
   return (
     <main>
-      <Navbar />
+      <Navbar cart={store.getState()} />
       <CartContainer cart={cartItems} />
     </main>
   );
